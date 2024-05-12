d3f44ec8d192d4261d4996cb2500a2d2fb8b9e5a
Eduardo Caires Silva <yandudu743@gmail.com>
2024-05-12 07:28:00 -0300

    Correção de páginas

--- src/components/LinkList/index.jsx
+++
@@ -1,9 +0,0 @@
-import './LinkList.css';
-
-export default function LinkList({ children }) {
-    return (
-        <div className="link-list">
-            { children }
-        </div>
-    );
-}
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ProjetoDctree"
+  base: "/ProjetoDctree",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, 'src')
